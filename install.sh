a4f70de51d1853a685f73430d253e629ba2f7ff0
Géraud Le Falher <daureg@gmail.com>
2010-05-18 13:09:12 +0200

    script pour installer tout ces fichiers

---
+++ install.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+ln -sfv $HOME/devel/dot-files/gvimrc $HOME/.gvimrc
+ln -sfv $HOME/devel/dot-files/vimrc $HOME/.vimrc
+ln -sfv $HOME/devel/dot-files/xinitrc $HOME/.xinitrc
+ln -sfv $HOME/devel/dot-files/bashrc $HOME/.bashrc
+ln -sfv $HOME/devel/dot-files/bash_profile $HOME/.bash_profile
+ln -sfv $HOME/devel/dot-files/bash_aliases $HOME/.bash_aliases
