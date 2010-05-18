2b74aa84734fc84b6e574a7fe0a8581e58496f27
Géraud Le Falher <daureg@gmail.com>
2010-05-18 13:10:47 +0200

    ajout des fichiers pour X, git et valgrind

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ ln -sfv $HOME/devel/dot-files/xinitrc $HOME/.xinitrc
 ln -sfv $HOME/devel/dot-files/bashrc $HOME/.bashrc
 ln -sfv $HOME/devel/dot-files/bash_profile $HOME/.bash_profile
 ln -sfv $HOME/devel/dot-files/bash_aliases $HOME/.bash_aliases
+ln -sfv $HOME/devel/dot-files/gitconfig $HOME/.git_config
+ln -sfv $HOME/devel/dot-files/valgrindrc $HOME/.valgrindrc
