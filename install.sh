4cb70c97cdd4c8614790a4fd8f36f984b412e704
Géraud Le Falher <daureg@gmail.com>
2014-02-27 15:33:41 +0200

    add pylintrc

--- install.sh
+++ install.sh
@@ -12,9 +12,11 @@ ln -svf $HOME/devel/dot-files/snippets/html-my.snippets ~/.vim/snippets/html-my.
 ln -sfv $HOME/devel/dot-files/gvimrc $HOME/.gvimrc
 ln -sfv $HOME/devel/dot-files/vimrc $HOME/.vimrc
 ln -sfv $HOME/devel/dot-files/xinitrc $HOME/.xinitrc
+ln -sfv $HOME/devel/dot-files/xserverrc $HOME/.xserverrc
 ln -sfv $HOME/devel/dot-files/bashrc $HOME/.bashrc
 ln -sfv $HOME/devel/dot-files/bash_profile $HOME/.bash_profile
 ln -sfv $HOME/devel/dot-files/bash_aliases $HOME/.bash_aliases
 ln -sfv $HOME/devel/dot-files/gitconfig $HOME/.git_config
 ln -sfv $HOME/devel/dot-files/valgrindrc $HOME/.valgrindrc
 ln -sfv $HOME/devel/dot-files/astylerc $HOME/.astylerc
+ln -sfv $HOME/devel/dot-files/pylintrc $HOME/.pylintrc
